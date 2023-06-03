45473037036718f68c99a02fb4383966f81ea504
Aman Jain <aman2012546@gmail.com>
2023-06-03 13:42:23 +0530

    After adding test case for 2nd scenario, it is dumping in line 12

--- Data Structures/Linked List/LinkedListCRUDOperations/LinkedListCRUDOperations/LinkedListCrudTest.cpp
+++ Data Structures/Linked List/LinkedListCRUDOperations/LinkedListCRUDOperations/LinkedListCrudTest.cpp
@@ -2,11 +2,12 @@
 #include "LinkedListCRUDOperations.h"
 
 TEST(LinkedListCRUDOperations, create_new_node) {
-	ListNode* root = create_node(10);
-	EXPECT_EQ(root->data, 10);
+	ListNode* root = create_node(0);
+	EXPECT_EQ(root->data, 0);
 }
 
 TEST(LinkedListCRUDOperations, add_node_to_front) {
-	ListNode *root = create_node(10);
-	root = add_node_to_front(root, create_node(20));
+	ListNode *root = create_node(0);
+	root = add_node_to_front(root, create_node(1));
+	EXPECT_EQ(root->data, 1);
 }
