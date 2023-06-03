83c3d81995741d517a7ecbc08c7e5d4ef770a9f7
Aman Jain <aman2012546@gmail.com>
2023-06-03 16:04:29 +0530

    Adding code to pass the test add_node_after_given_node_in_list

--- Data Structures/Linked List/LinkedListCRUDOperations/LinkedListCRUDOperations/LinkedListCRUDOperations.cpp
+++ Data Structures/Linked List/LinkedListCRUDOperations/LinkedListCRUDOperations/LinkedListCRUDOperations.cpp
@@ -14,5 +14,6 @@ ListNode* add_node_after(ListNode* root, ListNode* new_node) {
 	if (!root) {
 		return add_node_to_front(root, new_node);
 	}
-	return nullptr;
+	root->next = new_node;
+	return new_node;
 }
